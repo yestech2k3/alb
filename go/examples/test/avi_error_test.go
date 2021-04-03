345c327affd9e213b35ca0efc6782dbf629a5b39
Gaurav Rastogi <rastogiga@vmware.com>
2021-04-03 16:19:10 +0000

    Changed avientworks/sdk to vmware/alb-sdk

--- go/examples/test/avi_error_test.go
+++ go/examples/test/avi_error_test.go
@@ -4,8 +4,8 @@ import (
 	"os"
 	"testing"
 
-	"github.com/avinetworks/sdk/go/clients"
-	"github.com/avinetworks/sdk/go/session"
+	"github.com/vmware/alb-sdk/go/clients"
+	"github.com/vmware/alb-sdk/go/session"
 )
 
 func TestAviError(t *testing.T) {
