21078303a13ba85d93ddd620bdb74042d9e2997d
Yograj Shisode <yshisode@vmware.com>
2021-07-21 20:47:40 -0700

    Updated assets for go models and clients
    Signed-off-by: Yograj Shisode <yshisode@vmware.com>

--- go/models/portal_feature_opt_in.go
+++ go/models/portal_feature_opt_in.go
@@ -11,24 +11,15 @@ type PortalFeatureOptIn struct {
 	// Enable to subscribe to automated Application Signature Rulesets updates. Field introduced in 20.1.4. Allowed in Basic(Allowed values- false) edition, Essentials(Allowed values- false) edition, Enterprise edition.
 	EnableAppsignatureSync *bool `json:"enable_appsignature_sync,omitempty"`
 
-	// Enable pro-active support case creation when a service engine failure occurs. Field introduced in 20.1.1. Allowed in Basic(Allowed values- false) edition, Essentials(Allowed values- false) edition, Enterprise edition.
-	EnableAutoCaseCreationOnSeFailure *bool `json:"enable_auto_case_creation_on_se_failure,omitempty"`
-
-	// Enable pro-active support case creation when a system failure occurs. Manual download will still be available in the customer portal. Field introduced in 20.1.1. Allowed in Basic(Allowed values- false) edition, Essentials(Allowed values- false) edition, Enterprise edition.
-	EnableAutoCaseCreationOnSystemFailure *bool `json:"enable_auto_case_creation_on_system_failure,omitempty"`
-
-	// Enable to automatically download new CRS version to the Controller. Field introduced in 20.1.1. Allowed in Basic(Allowed values- false) edition, Essentials(Allowed values- false) edition, Enterprise edition.
-	EnableAutoDownloadWafSignatures *bool `json:"enable_auto_download_waf_signatures,omitempty"`
-
 	// Enable to subscribe to IP reputation updates. This is a requirement for using IP reputation in the product. Field introduced in 20.1.1. Allowed in Basic(Allowed values- false) edition, Essentials(Allowed values- false) edition, Enterprise edition.
 	EnableIPReputation *bool `json:"enable_ip_reputation,omitempty"`
 
-	// Subscribe to upload Controller Metrics and System Information to Pulse Portal. Field introduced in 21.1.1. Allowed in Basic(Allowed values- false) edition, Essentials(Allowed values- false) edition, Enterprise edition.
-	EnableSysteminfoCollection *bool `json:"enable_systeminfo_collection,omitempty"`
+	// Enable Pulse Case Management. Field introduced in 21.1.1. Allowed in Basic(Allowed values- false) edition, Essentials(Allowed values- false) edition, Enterprise edition. Special default for Basic edition is false, Essentials edition is false, Enterprise is True.
+	EnablePulseCaseManagement *bool `json:"enable_pulse_case_management,omitempty"`
+
+	// Enable Pulse WAF Management. Field introduced in 21.1.1. Allowed in Basic(Allowed values- false) edition, Essentials(Allowed values- false) edition, Enterprise edition. Special default for Basic edition is false, Essentials edition is false, Enterprise is True.
+	EnablePulseWafManagement *bool `json:"enable_pulse_waf_management,omitempty"`
 
 	// Enable subscription to User-Agent database used for Bot Management. Field introduced in 21.1.1. Allowed in Basic(Allowed values- false) edition, Essentials(Allowed values- false) edition, Enterprise edition.
 	EnableUserAgentDbSync *bool `json:"enable_user_agent_db_sync,omitempty"`
-
-	// Enable event notifications when new CRS versions are available. Field introduced in 20.1.1. Allowed in Basic(Allowed values- false) edition, Essentials(Allowed values- false) edition, Enterprise edition. Special default for Basic edition is false, Essentials edition is false, Enterprise is True.
-	EnableWafSignaturesNotifications *bool `json:"enable_waf_signatures_notifications,omitempty"`
 }
