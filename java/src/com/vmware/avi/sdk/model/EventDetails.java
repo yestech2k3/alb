f8a35191e2c662d3b11121a15f3fa6fba74d7a8d
Yograj Shisode <yshisode@vmware.com>
2021-11-02 20:34:01 -0700

    Updated assets for java
    Signed-off-by: Yograj Shisode <yshisode@vmware.com>

--- java/src/com/vmware/avi/sdk/model/EventDetails.java
+++ java/src/com/vmware/avi/sdk/model/EventDetails.java
@@ -309,11 +309,23 @@ public class EventDetails  {
     @JsonProperty("network_subnet_details")
     private NetworkSubnetInfo networkSubnetDetails = null;
 
+    @JsonProperty("nsxt_endpoint_details")
+    private NsxtSIEndpointDetails nsxtEndpointDetails = null;
+
     @JsonProperty("nsxt_img_details")
     private NsxtImageDetails nsxtImgDetails = null;
 
     @JsonProperty("nsxt_info")
     private NsxtSetup nsxtInfo = null;
+
+    @JsonProperty("nsxt_policy_details")
+    private NsxtSIPolicyDetails nsxtPolicyDetails = null;
+
+    @JsonProperty("nsxt_rule_details")
+    private NsxtSIRuleDetails nsxtRuleDetails = null;
+
+    @JsonProperty("nsxt_service_details")
+    private NsxtSIServiceDetails nsxtServiceDetails = null;
 
     @JsonProperty("nw_subnet_clash_details")
     private NetworkSubnetClash nwSubnetClashDetails = null;
@@ -2537,6 +2549,28 @@ public class EventDetails  {
 
     /**
      * This is the getter method this will return the attribute value.
+     * Nsx-t serviceinsertion virtualendpoint event.
+     * Field introduced in 20.1.8.
+     * Default value when not specified in API or module is interpreted by Avi Controller as null.
+     * @return nsxtEndpointDetails
+     */
+    public NsxtSIEndpointDetails getNsxtEndpointDetails() {
+        return nsxtEndpointDetails;
+    }
+
+    /**
+     * This is the setter method to the attribute.
+     * Nsx-t serviceinsertion virtualendpoint event.
+     * Field introduced in 20.1.8.
+     * Default value when not specified in API or module is interpreted by Avi Controller as null.
+     * @param nsxtEndpointDetails set the nsxtEndpointDetails.
+     */
+    public void setNsxtEndpointDetails(NsxtSIEndpointDetails nsxtEndpointDetails) {
+        this.nsxtEndpointDetails = nsxtEndpointDetails;
+    }
+
+    /**
+     * This is the getter method this will return the attribute value.
      * Nsxt image event.
      * Field introduced in 20.1.1.
      * Default value when not specified in API or module is interpreted by Avi Controller as null.
@@ -2577,6 +2611,72 @@ public class EventDetails  {
      */
     public void setNsxtInfo(NsxtSetup nsxtInfo) {
         this.nsxtInfo = nsxtInfo;
+    }
+
+    /**
+     * This is the getter method this will return the attribute value.
+     * Nsx-t serviceinsertion redirectpolicy event.
+     * Field introduced in 20.1.8.
+     * Default value when not specified in API or module is interpreted by Avi Controller as null.
+     * @return nsxtPolicyDetails
+     */
+    public NsxtSIPolicyDetails getNsxtPolicyDetails() {
+        return nsxtPolicyDetails;
+    }
+
+    /**
+     * This is the setter method to the attribute.
+     * Nsx-t serviceinsertion redirectpolicy event.
+     * Field introduced in 20.1.8.
+     * Default value when not specified in API or module is interpreted by Avi Controller as null.
+     * @param nsxtPolicyDetails set the nsxtPolicyDetails.
+     */
+    public void setNsxtPolicyDetails(NsxtSIPolicyDetails nsxtPolicyDetails) {
+        this.nsxtPolicyDetails = nsxtPolicyDetails;
+    }
+
+    /**
+     * This is the getter method this will return the attribute value.
+     * Nsx-t serviceinsertion redirectrule event.
+     * Field introduced in 20.1.8.
+     * Default value when not specified in API or module is interpreted by Avi Controller as null.
+     * @return nsxtRuleDetails
+     */
+    public NsxtSIRuleDetails getNsxtRuleDetails() {
+        return nsxtRuleDetails;
+    }
+
+    /**
+     * This is the setter method to the attribute.
+     * Nsx-t serviceinsertion redirectrule event.
+     * Field introduced in 20.1.8.
+     * Default value when not specified in API or module is interpreted by Avi Controller as null.
+     * @param nsxtRuleDetails set the nsxtRuleDetails.
+     */
+    public void setNsxtRuleDetails(NsxtSIRuleDetails nsxtRuleDetails) {
+        this.nsxtRuleDetails = nsxtRuleDetails;
+    }
+
+    /**
+     * This is the getter method this will return the attribute value.
+     * Nsx-t serviceinsertion service event.
+     * Field introduced in 20.1.8.
+     * Default value when not specified in API or module is interpreted by Avi Controller as null.
+     * @return nsxtServiceDetails
+     */
+    public NsxtSIServiceDetails getNsxtServiceDetails() {
+        return nsxtServiceDetails;
+    }
+
+    /**
+     * This is the setter method to the attribute.
+     * Nsx-t serviceinsertion service event.
+     * Field introduced in 20.1.8.
+     * Default value when not specified in API or module is interpreted by Avi Controller as null.
+     * @param nsxtServiceDetails set the nsxtServiceDetails.
+     */
+    public void setNsxtServiceDetails(NsxtSIServiceDetails nsxtServiceDetails) {
+        this.nsxtServiceDetails = nsxtServiceDetails;
     }
 
     /**
@@ -4741,6 +4841,10 @@ public class EventDetails  {
   Objects.equals(this.nsxtInfo, objEventDetails.nsxtInfo)&&
   Objects.equals(this.nsxtImgDetails, objEventDetails.nsxtImgDetails)&&
   Objects.equals(this.vcenterClusterDetails, objEventDetails.vcenterClusterDetails)&&
+  Objects.equals(this.nsxtServiceDetails, objEventDetails.nsxtServiceDetails)&&
+  Objects.equals(this.nsxtEndpointDetails, objEventDetails.nsxtEndpointDetails)&&
+  Objects.equals(this.nsxtPolicyDetails, objEventDetails.nsxtPolicyDetails)&&
+  Objects.equals(this.nsxtRuleDetails, objEventDetails.nsxtRuleDetails)&&
   Objects.equals(this.psmProgramDetails, objEventDetails.psmProgramDetails)&&
   Objects.equals(this.secMgrDataEvent, objEventDetails.secMgrDataEvent)&&
   Objects.equals(this.cloudAsgNotifDetails, objEventDetails.cloudAsgNotifDetails)&&
@@ -4855,8 +4959,12 @@ public class EventDetails  {
                         sb.append("    mgmtNwChangeDetails: ").append(toIndentedString(mgmtNwChangeDetails)).append("\n");
                         sb.append("    modifyNetworksDetails: ").append(toIndentedString(modifyNetworksDetails)).append("\n");
                         sb.append("    networkSubnetDetails: ").append(toIndentedString(networkSubnetDetails)).append("\n");
+                        sb.append("    nsxtEndpointDetails: ").append(toIndentedString(nsxtEndpointDetails)).append("\n");
                         sb.append("    nsxtImgDetails: ").append(toIndentedString(nsxtImgDetails)).append("\n");
                         sb.append("    nsxtInfo: ").append(toIndentedString(nsxtInfo)).append("\n");
+                        sb.append("    nsxtPolicyDetails: ").append(toIndentedString(nsxtPolicyDetails)).append("\n");
+                        sb.append("    nsxtRuleDetails: ").append(toIndentedString(nsxtRuleDetails)).append("\n");
+                        sb.append("    nsxtServiceDetails: ").append(toIndentedString(nsxtServiceDetails)).append("\n");
                         sb.append("    nwSubnetClashDetails: ").append(toIndentedString(nwSubnetClashDetails)).append("\n");
                         sb.append("    nwSummarizedDetails: ").append(toIndentedString(nwSummarizedDetails)).append("\n");
                         sb.append("    ociInfo: ").append(toIndentedString(ociInfo)).append("\n");
---
+++ java/src/com/vmware/avi/sdk/model/NsxtSIEndpointDetails.java
@@ -0,0 +1,246 @@
+/*
+ * Copyright 2021 VMware, Inc.
+ * SPDX-License-Identifier: Apache License 2.0
+ */
+
+package com.vmware.avi.sdk.model;
+
+import java.util.*;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
+
+/**
+ * The NsxtSIEndpointDetails is a POJO class extends AviRestResource that used for creating
+ * NsxtSIEndpointDetails.
+ *
+ * @version 1.0
+ * @since 
+ *
+ */
+@JsonIgnoreProperties(ignoreUnknown = true)
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class NsxtSIEndpointDetails  {
+    @JsonProperty("endpoint")
+    private String endpoint = null;
+
+    @JsonProperty("error_string")
+    private String errorString = null;
+
+    @JsonProperty("segroup")
+    private String segroup = null;
+
+    @JsonProperty("services")
+    private List<String> services = null;
+
+    @JsonProperty("targetIps")
+    private List<String> targetIps = null;
+
+    @JsonProperty("tier1")
+    private String tier1 = null;
+
+
+
+    /**
+     * This is the getter method this will return the attribute value.
+     * Virtualendpoint path.
+     * Field introduced in 20.1.8.
+     * Default value when not specified in API or module is interpreted by Avi Controller as null.
+     * @return endpoint
+     */
+    public String getEndpoint() {
+        return endpoint;
+    }
+
+    /**
+     * This is the setter method to the attribute.
+     * Virtualendpoint path.
+     * Field introduced in 20.1.8.
+     * Default value when not specified in API or module is interpreted by Avi Controller as null.
+     * @param endpoint set the endpoint.
+     */
+    public void setEndpoint(String  endpoint) {
+        this.endpoint = endpoint;
+    }
+
+    /**
+     * This is the getter method this will return the attribute value.
+     * Error message.
+     * Field introduced in 20.1.8.
+     * Default value when not specified in API or module is interpreted by Avi Controller as null.
+     * @return errorString
+     */
+    public String getErrorString() {
+        return errorString;
+    }
+
+    /**
+     * This is the setter method to the attribute.
+     * Error message.
+     * Field introduced in 20.1.8.
+     * Default value when not specified in API or module is interpreted by Avi Controller as null.
+     * @param errorString set the errorString.
+     */
+    public void setErrorString(String  errorString) {
+        this.errorString = errorString;
+    }
+
+    /**
+     * This is the getter method this will return the attribute value.
+     * Serviceenginegroup name.
+     * Field introduced in 20.1.8.
+     * Default value when not specified in API or module is interpreted by Avi Controller as null.
+     * @return segroup
+     */
+    public String getSegroup() {
+        return segroup;
+    }
+
+    /**
+     * This is the setter method to the attribute.
+     * Serviceenginegroup name.
+     * Field introduced in 20.1.8.
+     * Default value when not specified in API or module is interpreted by Avi Controller as null.
+     * @param segroup set the segroup.
+     */
+    public void setSegroup(String  segroup) {
+        this.segroup = segroup;
+    }
+    /**
+     * This is the getter method this will return the attribute value.
+     * Services where endpoint refers.
+     * Field introduced in 20.1.8.
+     * Default value when not specified in API or module is interpreted by Avi Controller as null.
+     * @return services
+     */
+    public List<String> getServices() {
+        return services;
+    }
+
+    /**
+     * This is the setter method. this will set the services
+     * Services where endpoint refers.
+     * Field introduced in 20.1.8.
+     * Default value when not specified in API or module is interpreted by Avi Controller as null.
+     * @return services
+     */
+    public void setServices(List<String>  services) {
+        this.services = services;
+    }
+
+    /**
+     * This is the setter method this will set the services
+     * Services where endpoint refers.
+     * Field introduced in 20.1.8.
+     * Default value when not specified in API or module is interpreted by Avi Controller as null.
+     * @return services
+     */
+    public NsxtSIEndpointDetails addServicesItem(String servicesItem) {
+      if (this.services == null) {
+        this.services = new ArrayList<String>();
+      }
+      this.services.add(servicesItem);
+      return this;
+    }
+    /**
+     * This is the getter method this will return the attribute value.
+     * Endpoint target ips.
+     * Field introduced in 20.1.8.
+     * Default value when not specified in API or module is interpreted by Avi Controller as null.
+     * @return targetIps
+     */
+    public List<String> getTargetips() {
+        return targetIps;
+    }
+
+    /**
+     * This is the setter method. this will set the targetIps
+     * Endpoint target ips.
+     * Field introduced in 20.1.8.
+     * Default value when not specified in API or module is interpreted by Avi Controller as null.
+     * @return targetIps
+     */
+    public void setTargetips(List<String>  targetIps) {
+        this.targetIps = targetIps;
+    }
+
+    /**
+     * This is the setter method this will set the targetIps
+     * Endpoint target ips.
+     * Field introduced in 20.1.8.
+     * Default value when not specified in API or module is interpreted by Avi Controller as null.
+     * @return targetIps
+     */
+    public NsxtSIEndpointDetails addTargetipsItem(String targetIpsItem) {
+      if (this.targetIps == null) {
+        this.targetIps = new ArrayList<String>();
+      }
+      this.targetIps.add(targetIpsItem);
+      return this;
+    }
+
+    /**
+     * This is the getter method this will return the attribute value.
+     * Tier1 path.
+     * Field introduced in 20.1.8.
+     * Default value when not specified in API or module is interpreted by Avi Controller as null.
+     * @return tier1
+     */
+    public String getTier1() {
+        return tier1;
+    }
+
+    /**
+     * This is the setter method to the attribute.
+     * Tier1 path.
+     * Field introduced in 20.1.8.
+     * Default value when not specified in API or module is interpreted by Avi Controller as null.
+     * @param tier1 set the tier1.
+     */
+    public void setTier1(String  tier1) {
+        this.tier1 = tier1;
+    }
+
+
+    @Override
+    public boolean equals(java.lang.Object o) {
+      if (this == o) {
+          return true;
+      }
+      if (o == null || getClass() != o.getClass()) {
+          return false;
+      }
+      NsxtSIEndpointDetails objNsxtSIEndpointDetails = (NsxtSIEndpointDetails) o;
+      return   Objects.equals(this.segroup, objNsxtSIEndpointDetails.segroup)&&
+  Objects.equals(this.tier1, objNsxtSIEndpointDetails.tier1)&&
+  Objects.equals(this.errorString, objNsxtSIEndpointDetails.errorString)&&
+  Objects.equals(this.endpoint, objNsxtSIEndpointDetails.endpoint)&&
+  Objects.equals(this.services, objNsxtSIEndpointDetails.services)&&
+  Objects.equals(this.targetIps, objNsxtSIEndpointDetails.targetIps);
+    }
+
+    @Override
+    public String toString() {
+      StringBuilder sb = new StringBuilder();
+      sb.append("class NsxtSIEndpointDetails {\n");
+                  sb.append("    endpoint: ").append(toIndentedString(endpoint)).append("\n");
+                        sb.append("    errorString: ").append(toIndentedString(errorString)).append("\n");
+                        sb.append("    segroup: ").append(toIndentedString(segroup)).append("\n");
+                        sb.append("    services: ").append(toIndentedString(services)).append("\n");
+                        sb.append("    targetIps: ").append(toIndentedString(targetIps)).append("\n");
+                        sb.append("    tier1: ").append(toIndentedString(tier1)).append("\n");
+                  sb.append("}");
+      return sb.toString();
+    }
+
+    /**
+     * Convert the given object to string with each line indented by 4 spaces
+     * (except the first line).
+     */
+    private String toIndentedString(java.lang.Object o) {
+      if (o == null) {
+          return "null";
+      }
+      return o.toString().replace("\n", "\n    ");
+    }
+}
