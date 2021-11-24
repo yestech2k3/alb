02a7cc42a9942362cf0ee106c04e30b675f3d771
Megha Agarwal <agarwalme@vmware.com>
2021-11-24 16:25:19 +0530

    Update vs_converter.py

--- python/avi/migrationtools/nsxt_converter/vs_converter.py
+++ python/avi/migrationtools/nsxt_converter/vs_converter.py
@@ -1,23 +1,28 @@
 from avi.migrationtools.nsxt_converter import conversion_util
 
 
-def update_alb_type(lb_vs, alb_vs):
-    alb_vs['url'], alb_vs['uuid'] = conversion_util.get_obj_url_uuid(lb_vs['path'], lb_vs['unique_id'])
-    alb_vs['enabled'] = lb_vs['enabled']
+def update_alb_type(lb_vs, alb_vs,prefix):
+
+    alb_vs['enabled'] = lb_vs.get('enabled')
 
     alb_vs['services']=dict(
-        port=lb_vs['ports'][0]
+        port=lb_vs.get('ports')[0]
         )
-    alb_vs['pool_ref']=lb_vs['pool_path']
-    alb_vs['application_profile_ref']=lb_vs['application_profile_path']
+
+
     alb_vs['ssl_profile_ref']=lb_vs.get('client_ssl_profile_binding')
-    tenant = conversion_util.get_tenant_ref(alb_vs['url'])
     alb_vs['tenant_ref'] = conversion_util.get_object_ref('admin', 'tenant')
 
-def convert(alb_config, nsx_lb_config,cloud_name):
+
+
+def convert(alb_config, nsx_lb_config,cloud_name,prefix):
     alb_config['VirtualService'] = list()
     alb_config['vsvip']=list()
     for lb_vs in nsx_lb_config['LBVirtualServers']:
+        if prefix:
+            name='%s-%s' % (prefix,lb_vs['display_name'])
+        else :
+            name=lb_vs['display_name']
         vsvip=dict(
             ip_address=dict(
                 addr=lb_vs['ip_address'],
@@ -25,11 +30,11 @@ def convert(alb_config, nsx_lb_config,cloud_name):
         )
         )
         alb_vs = dict(
-            name=lb_vs['display_name'],
+            name=name,
 
         )
 
-        update_alb_type(lb_vs, alb_vs)
+        update_alb_type(lb_vs, alb_vs,prefix)
         alb_vs['cloud_ref'] = conversion_util.get_object_ref(cloud_name, 'cloud')
         alb_config['VirtualService'].append(alb_vs)
         alb_config['vsvip'].append(vsvip)
