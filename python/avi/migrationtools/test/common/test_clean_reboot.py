cb04b5975971f6c117689cb177045321d5f9d967
Mayur Khachane <khachanem@vmware.com>
2022-03-31 15:55:41 +0530

    AV-132375: Username password cannot be same (#2044)
    Co-authored-by: Chaitanya Deshpande <chaitanya.deshpande@avinetworks.com>

--- python/avi/migrationtools/test/common/test_clean_reboot.py
+++ python/avi/migrationtools/test/common/test_clean_reboot.py
@@ -35,7 +35,7 @@ def clean_reboot(controller_ip, username, password, version, licensefile_path):
         wait_until_node_ready (session)
         if version > "16.5.4" :
             session.clear_cached_sessions()
-            set_default_password(controller_ip, username)
+            set_default_password(controller_ip, username, password)
     else:
         raise Exception("Failed with error %s" % res.content)
     with open(licensefile_path, 'r') as license:
@@ -44,11 +44,13 @@ def clean_reboot(controller_ip, username, password, version, licensefile_path):
     upload_license(session, licensefile)
 
 
-def set_default_password(controller_ip, username):
+def set_default_password(controller_ip, username, password=None):
+    if not password:
+        password = 'Admin!23'
     api = ApiSession.get_session(controller_ip, username, password=os.environ['default_password'])
     passData = {
         "username": "admin",
-        "password": "admin",
+        "password": password,
         "old_password": os.environ['default_password'],
         'full_name': 'System Administrator',
     }
