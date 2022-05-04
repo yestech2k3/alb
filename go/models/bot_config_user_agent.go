ea423cb4caa8dc70d20485feaa409d35879c7b3d
Yograj Shisode <yshisode@vmware.com>
2022-05-04 10:06:56 -0700

    Updated assets for go models and clients
    Signed-off-by: Yograj Shisode <yshisode@vmware.com>

--- go/models/bot_config_user_agent.go
+++ go/models/bot_config_user_agent.go
@@ -10,4 +10,7 @@ type BotConfigUserAgent struct {
 
 	// Whether User Agent-based Bot detection is enabled. Field introduced in 21.1.1. Allowed in Enterprise edition with any value, Enterprise with Cloud Services edition.
 	Enabled *bool `json:"enabled,omitempty"`
+
+	// Whether to match the TLS fingerprint observed on the request against TLS fingerprints expected for the user agent. Field introduced in 22.1.1. Allowed in Enterprise edition with any value, Enterprise with Cloud Services edition.
+	UseTLSFingerprint *bool `json:"use_tls_fingerprint,omitempty"`
 }
