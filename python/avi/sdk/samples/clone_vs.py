c2648e6bd6ea4aecdc7bf497ee22bd2acaead00b
Roberto Casula <rcasula@vmware.com>
2022-06-14 15:58:55 +0100

    Update assets

--- python/avi/sdk/samples/clone_vs.py
+++ python/avi/sdk/samples/clone_vs.py
@@ -1,7 +1,14 @@
+#!/usr/bin/env python3
+
+############################################################################
+# ========================================================================
+# Copyright 2021 VMware, Inc.  All rights reserved. VMware Confidential
+# ========================================================================
+###
+
 # Copyright 2021 VMware, Inc.
 # SPDX-License-Identifier: Apache License 2.0
 
-#!/usr/bin/env python3
 
 from __future__ import print_function
 import sys
